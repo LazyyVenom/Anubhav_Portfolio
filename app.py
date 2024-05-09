ad065feccc7e857252171ca156bd6fb7a3efe91f
Anubhav <choubey.anubhav253@gmail.com>
2024-05-09 17:54:38 +0530

    added not in github

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/not_in_github")
+def not_in_github():
+    return render_template("not_in_github.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
